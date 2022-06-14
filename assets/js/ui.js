6ff4dbc4b59fd01caff800e5d0170c3b3cabf575
daol0926 <dh.b0926@gmail.com>
2022-06-14 11:31:32 +0900

    KON적립 조기마감, 선착순마감 추가

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -461,14 +461,16 @@ function tabMenu() {
 		$(".tab-wrp .tab-menu li").removeClass("focused"); 
 		$(".tab-wrp .tab-cont").removeClass("focused"); 
 		$('.tab-wrp .tab-menu li:eq(' + num + ')').addClass("focused"); 
-		$('.tab-wrp .tab-cont:eq(' + num + ')').addClass("focused"); });
+		$('.tab-wrp .tab-cont:eq(' + num + ')').addClass("focused"); 
+	});
 	
-		$(".tab-wrp .inner-tab li a").on("click", function(){  
-			const num = $(".tab-wrp .inner-tab li a").index($(this)); 
-			$(".tab-wrp .inner-tab li").removeClass("focused"); 
-			$(".tab-wrp .inner-tab-cont").removeClass("focused"); 
-			$('.tab-wrp .inner-tab li:eq(' + num + ')').addClass("focused"); 
-			$('.tab-wrp .inner-tab-cont:eq(' + num + ')').addClass("focused"); });
+	$(".tab-wrp .inner-tab li a").on("click", function(){  
+		const num = $(".tab-wrp .inner-tab li a").index($(this)); 
+		$(".tab-wrp .inner-tab li").removeClass("focused"); 
+		$(".tab-wrp .inner-tab-cont").removeClass("focused"); 
+		$('.tab-wrp .inner-tab li:eq(' + num + ')').addClass("focused"); 
+		$('.tab-wrp .inner-tab-cont:eq(' + num + ')').addClass("focused"); 
+	});
 }
 
 /* 알람 list swipe */
@@ -497,6 +499,13 @@ function addBrand(){
 	$('.add-brand').click(function(){
 		$(this).hide();
 		$(this).parents().find('.brand-select').removeClass('select-complete').addClass('select-add');
+	});
+}
+
+/* desc info */
+function descInfo(){
+	$('.ico-desc-info').click(function(){
+		$(this).toggleClass('focused');
 	});
 }
 
