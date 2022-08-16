abd988a8747eef1f8f12022e519d857dbdbcfe64
daol0926 <dh.b0926@gmail.com>
2022-08-16 17:46:26 +0900

    cate sticky 높이값 인식 부분 수정

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -294,7 +294,7 @@ function cateSticky(){
 		});
     $(document).on('click','.cate-wrap button', function() {
 		var target = $(this).attr('data-target'),
-			offsetTop = $(target).offset().top - (headerGab+cateh);
+			offsetTop = $(target).offset().top - (headerGab+cateh) + 3;
 		menuItems.addClass("current").not(this).removeClass("current");
 		$('html, body').stop().animate({
 			scrollTop: offsetTop
